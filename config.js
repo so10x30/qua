985d146c046e95dbc577bc72a191a042aac0db5e
vinh trinh <haloa@DESKTOP-A86O54Q>
2022-01-02 21:57:41 +0700

    init file

---
+++ config.js
@@ -0,0 +1,17 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
+    introDesc: `t có điều này muốn hỏi m, m nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 't biết m thích t phải không phải không ? 😙',
+    desc: 'm không trả lời hoặc thoát ra, tức là m thích t đó nha :> ',
+    btnYes: 'Có, t thật sự thích m 💖💖💖',
+    btnNo: '💔 m mơ à ! 💔 ',
+    question:'Nói cho t biết đi vì sao m thích t 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì t thích hiii 😊😊',
+    mess: 't biết mà, t cũng thích m lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho t nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+}
